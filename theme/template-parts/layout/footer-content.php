a40c380818bdd1ee95a7e6d1962ff6f1ae1b0f93
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2023-10-11 22:11:27 -0700

    footer updates

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -8,40 +8,108 @@
  * @package wp-tailwind
  */
 
+use wp_tailwind\theme\Fields\ACF;
+
+$group_logo = get_field('group_logo', 'option');
+$logo = ACF::getField('footer_branding_logo', $group_logo);
+
+$group_links = get_field('group_links', 'option');
+$links = $group_links['footer_nav'];
+
+$group_location = get_field('group_location', 'option');
+$group_contact = get_field('group_contact', 'option');
+$group_social = get_field('group_social', 'option');
+
+$group_last_call = get_field('last_call_group', 'option');
+$last_call_title = ACF::getField('last_call_title', $group_last_call);
+$last_call_copy = ACF::getField('last_call_copy', $group_last_call);
+$last_cal_link = ACF::getField('last_cal_link', $group_last_call);
+
 ?>
 
-<footer id="colophon">
-
-	<?php if (has_nav_menu('menu-2')) : ?>
-		<nav aria-label="<?php esc_attr_e('Footer Menu', 'wp-tailwind'); ?>">
+<footer id="colophon" class="footer">
+	<div class="last-call">
+		<div class="md:w-[499px] w-[85%] mid:mx-0 mx-auto">
+			<h2 class="lc-title"><?php echo __($last_call_title); ?></h2>
+			<div class="lc-copy">
+				<?php echo apply_filters('the_content', $last_call_copy); ?>
+			</div>
+		</div>
+		<div class="lc-link">
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-2',
-					'menu_class'     => 'footer-menu',
-					'depth'          => 1,
-				)
+			printf(
+				'
+				<a href="%2$s" target="%3$s" class="dps-light-btn">%1$s</a>
+				',
+				$last_cal_link['title'],
+				$last_cal_link['url'],
+				$last_cal_link['target'],
 			);
 			?>
-		</nav>
-	<?php endif; ?>
-
-	<div>
-		<?php
-		$wp_tailwind_blog_info = get_bloginfo('name');
-		if (!empty($wp_tailwind_blog_info)) :
-		?>
-			<a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>,
-		<?php
-		endif;
-
-		/* translators: 1: WordPress link, 2: WordPress. */
-		printf(
-			'<a href="%1$s">proudly powered by %2$s</a>.',
-			esc_url(__('https://wordpress.org/', 'wp-tailwind')),
-			'WordPress'
-		);
-		?>
+		</div>
 	</div>
-
+	<div class="footer-columns">
+		<div class="dps-wrapper">
+			<div class="fc-top">
+				<div class="fc-image">
+					<?php
+					printf(
+						'<img class="w-full" src="%1$s" />',
+						$logo['url']
+					);
+					?>
+				</div>
+				<div class="fc-nav">
+					<?php
+					echo "<ul>";
+					foreach ($links as $link) {
+						printf(
+							'<li><a href="%2$s" target="%3$s">%1$s</a></li>',
+							$link['nav_link']['title'],
+							$link['nav_link']['url'],
+							$link['nav_link']['target'],
+						);
+					}
+					echo "</ul>";
+					?>
+				</div>
+			</div>
+			<div class="fc-bottom">
+				<div class="fc-locations">
+					<?php
+					printf(
+						'
+							<h3>%1$s</h3>
+							<p>%2$s</p>
+							',
+						$group_location['location_section_title'],
+						apply_filters('the_content', $group_location['location_section_copy']),
+					);
+					?>
+				</div>
+				<div class="fc-contact">
+					<?php
+					printf(
+						'
+							<h3>%1$s</h3>
+							<p>%2$s</p>
+							',
+						$group_contact['contact_title'],
+						apply_filters('the_content', $group_contact['contact_copy']),
+					);
+					?>
+				</div>
+				<div class="fc-social">
+					<?php
+					foreach ($group_social['social_icons'] as $icon) {
+						printf(
+							'<i class="%1$s"></i>',
+							$icon['icons']
+						);
+					}
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer><!-- #colophon -->
