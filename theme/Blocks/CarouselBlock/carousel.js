38c1905a82579fc52661437d0d5c4163baefecfa
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2023-10-12 15:38:33 -0700

    added carousel block

---
+++ theme/Blocks/CarouselBlock/carousel.js
@@ -0,0 +1,31 @@
+/**
+ * Front-end JavaScript
+ *
+ * The JavaScript code you place here will be processed by esbuild. The output
+ * file will be created at `../theme/js/script.min.js` and enqueued in
+ * `../theme/functions.php`.
+ *
+ * For esbuild documentation, please see:
+ * https://esbuild.github.io/
+ */
+/* global wp */
+import Swiper from 'swiper';
+
+export const CarouselTest = () => {
+	const swiper = new Swiper('.swiper', {
+		direction: 'horizontal',
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+		},
+	});
+
+	return swiper;
+};
